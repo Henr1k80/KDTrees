ab8f35db35534af1f551be4e40fc00abc2ef1357
Stephan Møller <stephanryer@hotmail.com>
2022-02-08 00:22:39 +0100

    Rearranged program.cs

--- KDTrees/Program.cs
+++ KDTrees/Program.cs
@@ -3,64 +3,60 @@ using KDTrees;
 using KDTrees.Strategies;
 using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"{DateTime.Now} Building map and points to check");
+var map = new MapOfPoints(GenerateRandomUniquePoints(pointCount: 10_000_000));
+var pointsToCheck = GenerateRandomUniquePoints(pointCount: 10_000);
 
-int pointMaxValue = 1_000_000;
+Console.WriteLine($"{DateTime.Now} Building indexes");
+var treeStrategy = new TreeStrategy();
+treeStrategy.BuildIndex(map);
 
-int pointCountOnMap = 10_000_000;
-Console.WriteLine($"{DateTime.Now} Creating a list of {pointCountOnMap} random points");
-var pointsOnMap = new HashSet<Point>(capacity: pointCountOnMap);
-var ran = new Random();
-while (pointsOnMap.Count < pointCountOnMap)
-{
-    pointsOnMap.Add(new Point(x: ran.Next(-pointMaxValue, pointMaxValue), y: ran.Next(-pointMaxValue, pointMaxValue)));
-}
-var map = new MapOfPoints(pointsOnMap);
-
-var pointCountToCheck = 10_000;
-Console.WriteLine($"{DateTime.Now} Create a random list of {pointCountOnMap} points to check");
-var pointsToCheck = new List<Point>();
-for (var i = 0; i < pointCountToCheck; i++)
-{
-    pointsToCheck.Add(new Point(x: ran.Next(-pointMaxValue, pointMaxValue), y: ran.Next(-pointMaxValue, pointMaxValue)));
-}
-
-Console.WriteLine($"{DateTime.Now} Building index");
-var strategies = new List<IClosestPointFindStrategy>();
-//strategies.Add(new DirtyStrategy());
-strategies.Add(new TreeStrategy());
-var stopwatch = new Stopwatch();
-foreach (var strategy in strategies)
-{
-    stopwatch.Restart();
-    strategy.BuildIndex(map);
-    Console.WriteLine($"{DateTime.Now} Strategy {strategy.GetType().Name} initiated in {stopwatch.ElapsedMilliseconds}ms");
-}
-
-// VALIDATE A given strategy compared to the dirty strategy
-//Console.WriteLine("Starting to check same value as dirty");
-//while(true){
-//    var checkPoint = new Point(x: ran.Next(-pointMaxValue, pointMaxValue), y: ran.Next(-pointMaxValue, pointMaxValue));
-//    var result1 = strategies[0].FindClosestPoints(checkPoint);
-//    var result2 = strategies[1].FindClosestPoints(checkPoint);
-//    if (result1.Distance != result2.Distance)
-//        throw new Exception("Not the same distance!");
-
-//    if (result1.ClosestPoints.Count != result2.ClosestPoints.Count)
-//        throw new Exception("Not the same point count!");
-//    Console.Write(".");
-//}
-
-Console.WriteLine($"{DateTime.Now} Starting comparison");
-foreach (var strategy in strategies)
-{
-    Console.WriteLine($"Testing strategy {strategy.GetType().Name}...");
-    stopwatch.Restart();
-    foreach (var p in pointsToCheck)
-    {
-        strategy.FindClosestPoints(p);
-    }
-    Console.WriteLine($"Strategy {strategy.GetType().Name} checked all {pointsToCheck.Count} points in {stopwatch.ElapsedMilliseconds}ms");
-}
+// To test if a given strategy works, out-comment the following to test it against the simple and correct, but slow strategy, DirtyStrategy
+// RunStrategyAndCompareWithDirtyToCheckIfErrorsCanBeFound(strategyToTest: treeStrategy, map: map);
+CheckAllPointsAgainstStrategy(strategyToTest: treeStrategy, pointsToCheck: pointsToCheck);
 
 Console.ReadLine();
+
+static void CheckAllPointsAgainstStrategy(IClosestPointFindStrategy strategyToTest, HashSet<Point> pointsToCheck)
+{
+    Console.WriteLine($"{DateTime.Now} Testing strategy {strategyToTest.GetType().Name}...");
+    var stopwatch = Stopwatch.StartNew();
+    foreach (var p in pointsToCheck)
+    {
+        strategyToTest.FindClosestPoints(p);
+    }
+    Console.WriteLine($"{DateTime.Now} Strategy {strategyToTest.GetType().Name} checked all {pointsToCheck.Count} points in {stopwatch.ElapsedMilliseconds}ms");
+
+}
+
+static void RunStrategyAndCompareWithDirtyToCheckIfErrorsCanBeFound(IClosestPointFindStrategy strategyToTest, MapOfPoints map)
+{
+    // VALIDATE A given strategy compared to the dirty strategy
+    Console.WriteLine($"{DateTime.Now} Starting to check same value as dirty");
+    while (true)
+    {
+        var dirtyStrategy = new DirtyStrategy();
+        dirtyStrategy.BuildIndex(map);
+        var checkPoint = GenerateRandomUniquePoints(pointCount: 1).Single();
+        var result1 = dirtyStrategy.FindClosestPoints(checkPoint);
+        var result2 = strategyToTest.FindClosestPoints(checkPoint);
+        if (result1.Distance != result2.Distance)
+            throw new Exception("Not the same distance!");
+
+        if (result1.ClosestPoints.Count != result2.ClosestPoints.Count)
+            throw new Exception("Not the same point count!");
+        Console.Write(".");
+    }
+}
+
+static HashSet<Point> GenerateRandomUniquePoints(int pointCount)
+{
+    int pointMaxValue = 1_000_000;
+    var pointsToCheck = new HashSet<Point>();
+    var ran = new Random();
+    while (pointsToCheck.Count < pointCount)
+    {
+        pointsToCheck.Add(new Point(x: ran.Next(-pointMaxValue, pointMaxValue), y: ran.Next(-pointMaxValue, pointMaxValue)));
+    }
+    return pointsToCheck;
+}
