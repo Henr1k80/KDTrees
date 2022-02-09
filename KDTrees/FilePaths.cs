2beff86ac5845594773459d5a85afdc279395636
Henrik Gedionsen <hg@impact.dk>
2022-02-09 08:09:09 +0100

    Made project location agnostic

--- KDTrees/FilePaths.cs
+++ KDTrees/FilePaths.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace KDTrees;
 
-namespace KDTrees
+public static class FilePaths
 {
-    public static class FilePaths
-    {
-        public const string MapPointsFilePath = "C:\\git\\KDTrees\\points_map.txt";
-        public const string CheckPointsFilePath = "C:\\git\\KDTrees\\points_check.txt";
-    }
+    public const string MapPointsFilePath = "..\\..\\..\\..\\points_map.txt";
+    public const string CheckPointsFilePath = "..\\..\\..\\..\\points_check.txt";
 }
